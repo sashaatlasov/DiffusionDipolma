63b618a71875db069d411e5713e1d4cd26c38211
Atlasov Alexander <sasha.atlasov@mail.ru>
2025-04-25 18:31:33 +0300

    general pipeline fixes

--- diffusion.py
+++ diffusion.py
@@ -161,8 +161,8 @@ def timestep_to_alpha(timesteps, T):
 
 def get_cosine_schedules(num_timesteps: int) -> Dict[str, torch.Tensor]:
 
-    alphas_cumprod = torch.tensor(timestep_to_alpha(np.arange(0, num_timesteps + 1), num_timesteps + 1))
-    betas = torch.tensor(1 - alphas_cumprod[1:] / alphas_cumprod[:-1])
+    alphas_cumprod = torch.tensor(timestep_to_alpha(np.arange(0, num_timesteps + 1), num_timesteps + 1), dtype=torch.float32)
+    betas = 1 - alphas_cumprod[1:] / alphas_cumprod[:-1]
     alphas_cumprod = alphas_cumprod[:-1]
     sqrt_betas = torch.sqrt(betas)
     alphas = 1 - betas
